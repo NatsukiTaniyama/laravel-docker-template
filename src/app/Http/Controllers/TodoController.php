29a3d99c4a0789a7cbad41d34a2ffb610ff7eef2
谷山夏樹 <taniyamanatsuki@taniyamanatsukinoMacBook-Pro.local>
2025-01-30 19:31:29 +0900

    bladeファイルを指定

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+
 use App\Todo;
 
 class TodoController extends Controller
@@ -12,6 +13,6 @@ class TodoController extends Controller
         $todo = new Todo();
         $todos = $todo->all();
         
-        return view('todo.index');
+        return view('todo.index', ['todos' => '$todos']);
     }
 }
